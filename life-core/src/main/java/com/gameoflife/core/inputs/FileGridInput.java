7211cf2664fe733ecd7d72c897d9fd1882459ed1
mtazi <m.tazi@egencia.com>
2015-08-17 16:06:54 +0200

    fileGridInput add

--- life-core/src/main/java/com/gameoflife/core/inputs/FileGridInput.java
+++ life-core/src/main/java/com/gameoflife/core/inputs/FileGridInput.java
@@ -2,10 +2,23 @@ package com.gameoflife.core.inputs;
 
 import com.gameoflife.core.models.Cell;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Created by Mehdi on 17/08/2015.
  */
+
 public class FileGridInput implements  GridInput {
+
+    //java 7 features
+    public FileGridInput(String fileName){
+
+        //read file using the "using" keyword in order to automatically close  the file handle
+        // put exception in one line using ||
+    }
 
     public int getWidth() {
         return 0;
